547af4e615b34bf6ff13395692f6fa06e8650481
AlejandraPachonVargas <alejandralesha@gmail.com>
2023-03-05 12:03:06 -0500

    'implementación de excepciones'

--- creaciónEtiquetas.py
+++ creaciónEtiquetas.py
@@ -5,17 +5,33 @@ diccionarioEtiquetas={}
 
 #crea las etiquetas 
 def crearEtiquetas(nombreEtiqueta):
-    listEtiquetas.append(nombreEtiqueta)
+    if nombreEtiqueta in listEtiquetas:
+        print('La etiqueta ya existe')
+        pass
+    else:
+        listEtiquetas.append(nombreEtiqueta)
     return listEtiquetas
 
 #modificar las etiquetas
 def modificarEtiquetas(oldEtiqueta,newEtiqueta):
-    listEtiquetas[listEtiquetas.index(oldEtiqueta)]=newEtiqueta
+    if oldEtiqueta in listEtiquetas:
+        if (newEtiqueta in listEtiquetas):
+            print('la etiqueta ya existe')
+            pass
+        else:
+            listEtiquetas[listEtiquetas.index(oldEtiqueta)]=newEtiqueta
+    else:
+        print('La etiqueta que desea modificar no existe')
+        pass
     return listEtiquetas
 
 #eliminar las etiquetas
 def eliminarEtiquetas(etiquetaEliminar):
-    listEtiquetas.remove(etiquetaEliminar)
+    if etiquetaEliminar in listEtiquetas:
+        listEtiquetas.remove(etiquetaEliminar)
+    else:
+        print('La etiqueta que desea eliminar no existe')
+        pass
     return listEtiquetas
 
 #llenar las etiquetas con los canales
@@ -32,6 +48,8 @@ def llenarEtiquetas(etiqueta,numeros):
 if __name__=="__main__":     
     print(crearEtiquetas('salud'))
     print(crearEtiquetas('cosina'))
+    print(crearEtiquetas('depirtes'))
     print(modificarEtiquetas('cosina','cocina'))
+    print(modificarEtiquetas('depirtes','deportes'))
     print(eliminarEtiquetas('salud'))
     print(llenarEtiquetas('cocina','1 2 3 4'))
