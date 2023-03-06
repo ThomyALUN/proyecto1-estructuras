3dfe3de96c912845ba70770e84cc3d74533b84d4
jgaviri <jgaviriat@unal.edu.co>
2023-03-05 19:00:47 -0500

    Dos ventanas corriendo juntas

--- interfazC.py
+++ interfazC.py
@@ -1,25 +1,17 @@
-import sys
-import os
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QFileDialog, QDialog
-from imagen import *
 
-
-
-
-class WelcomeScreen(QMainWindow):
+import sys
+class DisenoInicio(QMainWindow):
     def __init__(self):
-        super(WelcomeScreen, self).__init__()
+        super().__init__()
         loadUi("DiseñoInicio.ui", self)
+
+        # Agregar un botón que muestra la nueva ventana
         self.cerrar.clicked.connect(self.exit)
         self.min.clicked.connect(self.minimizar)
         self.frame.mouseMoveEvent = self.moveWindow   
-        self.BIniciar.clicked.connect(self.iniciarVentana1)     
-    
-    
+        self.BIniciar.clicked.connect(self.mostrarVentana1)
         
     def moveWindow(self,e):
         if e.buttons() == Qt.LeftButton:
@@ -37,6 +29,8 @@ class WelcomeScreen(QMainWindow):
     def exit(self):
         app.exit()
         sys.exit()
+        
+    
         
         
     def moveWindow(self,e):
@@ -57,47 +51,27 @@ class WelcomeScreen(QMainWindow):
     
     def minimizar(self):        
         widget.showMinimized()
-    
-    def iniciarVentana1(self):
+        
+    def mostrarVentana1(self):
+        # Crear una instancia de la nueva ventana
+        
         ventana1 = Ventana1()
         ventana1.show()
-        ventana1.raise_()
-        '''widget_2 = QtWidgets.QStackedWidget()
-        widget_2.addWidget(ventana1)
-        widget_2.setFixedHeight(768)
-        widget_2.setFixedWidth(1366)
-        widget_2.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        widget_2.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        widget_2.show()'''
+        
+        #self.ocultar()
+        
 
-        
-        
 class Ventana1(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("Ventana1.ui", self)
-        self.cerrar_2.clicked.connect(WelcomeScreen().ocultar)
-        self.min_2.clicked.connect(WelcomeScreen().minimizar)
-        self.frame_2.mouseMoveEvent = WelcomeScreen().moveWindow
         
-        
-        
+        # Definir la interfaz de usuario de la nueva ventana
+        # ...
 
-        
-app = QApplication(sys.argv)
-welcome = WelcomeScreen()
-widget = QtWidgets.QStackedWidget()
-widget_2 = QtWidgets.QStackedWidget()
-
-widget.addWidget(welcome)
-widget.setFixedHeight(768)
-widget.setFixedWidth(1366)
-widget.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-widget.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-widget.show()
-
-
-try: 
-    sys.exit(app.exec_())
-except:
-    print("Saliendo")
+if __name__ == '__main__':
+    app = QApplication([])
+    window = DisenoInicio()
+    window.show()
+    app.exec_()
+    
