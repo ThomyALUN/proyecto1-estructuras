8086d23e1e06c871829f90f631d7c2cb900d2278
BrahianStiven <becheverryq@unal.edu.co>
2023-03-06 22:36:17 -0500

    Excepciones temporales

--- creacion_csv.py
+++ creacion_csv.py
@@ -6,4 +6,24 @@ def convertir(df):
     return df
 df=archivo()
 print(convertir(df))
+notas="canales_noind.csv"
+try:
+    file = open(notas, "r")
+    content = file.read()
+    file.close()
+except FileNotFoundError:
+    print("El archivo no se pudo encontrar")
+except PermissionError:
+    print("No se tienen los permisos necesarios para acceder al archivo")
+except IOError:
+    print("Ocurrió un error de E/S al acceder al archivo")
+except Exception as e:
+    print("Ocurrió un error inesperado:", e)
+else:
+    print("El archivo se leyó correctamente")
+
+
+
+
+
 
